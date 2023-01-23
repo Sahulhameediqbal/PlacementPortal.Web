745a58d7c7e74859abdfea175221219030c41515
sahulhameed.iqbal <sahulhameed.iqbal@rencata.com>
2023-01-23 13:44:23 +0530

    Updated in Placement Page

---
+++ PlacementPortal.Web/Controllers/PlacementController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlacementPortal.Web.Controllers
+{
+    public class PlacementController : Controller
+    {
+        public IActionResult Placement()
+        {
+            return View("Placement");
+        }
+
+        [HttpGet]
+        public IActionResult AddPlacement()
+        {
+            return View("AddPlacement");
+        }
+    }
+}
